b34e5f2b71e2f17b2cc10c2ee22a77a40da2040f
Hennan Gadelha <hennanhennan@gmail.com>
2020-05-10 17:23:15 -0300

    criando projeto

---
+++ src/main/java/com/treinamentojavaweb/projetowebservicespringboot/ProjetowebservicespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentojavaweb.projetowebservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebservicespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebservicespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamentojavaweb/projetowebservicespringboot/ProjetowebservicespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentojavaweb.projetowebservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebservicespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
