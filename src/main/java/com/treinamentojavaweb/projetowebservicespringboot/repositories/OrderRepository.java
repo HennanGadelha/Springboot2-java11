460069457b0911165f8b95448fa70762c26002ec
Hennan Gadelha <hennanhennan@gmail.com>
2020-05-11 16:58:24 -0300

    Utilizando JsonIgnore para serializar arquivos na relaçao n x 1, por meio do jacksson

---
+++ src/main/java/com/treinamentojavaweb/projetowebservicespringboot/repositories/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.treinamentojavaweb.projetowebservicespringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.treinamentojavaweb.projetowebservicespringboot.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+	
+	
+	
+}
