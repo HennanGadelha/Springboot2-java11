1029987b593238881eb100f18cb9662387894ae5
Hennan Gadelha <hennanhennan@gmail.com>
2020-05-10 21:15:18 -0300

    registrando componentes

--- src/main/java/com/treinamentojavaweb/projetowebservicespringboot/resources/UserResource.java
+++ src/main/java/com/treinamentojavaweb/projetowebservicespringboot/resources/UserResource.java
@@ -1,22 +1,38 @@
 package com.treinamentojavaweb.projetowebservicespringboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.treinamentojavaweb.projetowebservicespringboot.entities.User;
+import com.treinamentojavaweb.projetowebservicespringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findall(){
+	public ResponseEntity<List<User>> findall(){
 		
-		User u = new User(1L, "Hennan", "hennan@gmail.com", "87499813", "12345");
+		List<User> users = service.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(users);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 		
 	}
 	
---
+++ src/main/java/com/treinamentojavaweb/projetowebservicespringboot/services/UserService.java
@@ -0,0 +1,35 @@
+package com.treinamentojavaweb.projetowebservicespringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.treinamentojavaweb.projetowebservicespringboot.entities.User;
+import com.treinamentojavaweb.projetowebservicespringboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> findAll(){
+		
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		return user.get();
+	}
+	
+	
+	
+	
+	
+}
