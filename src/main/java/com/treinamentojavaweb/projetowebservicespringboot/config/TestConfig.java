6d1a62681017d3498782adb8c9c621c64de53e5b
Hennan Gadelha <hennanhennan@gmail.com>
2020-05-14 20:22:30 -0300

    corrigindo bug ao serializar Json com  JsonIgnore

--- src/main/java/com/treinamentojavaweb/projetowebservicespringboot/config/TestConfig.java
+++ src/main/java/com/treinamentojavaweb/projetowebservicespringboot/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.treinamentojavaweb.projetowebservicespringboot.entities.Category;
 import com.treinamentojavaweb.projetowebservicespringboot.entities.Order;
+import com.treinamentojavaweb.projetowebservicespringboot.entities.OrderItem;
 import com.treinamentojavaweb.projetowebservicespringboot.entities.Product;
 import com.treinamentojavaweb.projetowebservicespringboot.entities.User;
 import com.treinamentojavaweb.projetowebservicespringboot.entities.enums.OrderStatus;
 import com.treinamentojavaweb.projetowebservicespringboot.repositories.CategoryRepository;
+import com.treinamentojavaweb.projetowebservicespringboot.repositories.OrderItemRepository;
 import com.treinamentojavaweb.projetowebservicespringboot.repositories.OrderRepository;
 import com.treinamentojavaweb.projetowebservicespringboot.repositories.ProductRepository;
 import com.treinamentojavaweb.projetowebservicespringboot.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -60,6 +65,7 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		
 		p1.getCategories().add(cat2);
 		
 		p2.getCategories().add(cat1);
@@ -72,6 +78,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 
